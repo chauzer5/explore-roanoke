5873c36e2b8cff74ac9a921860070688b016cccd
chauzer5 <chauzer.reed@gmail.com>
2024-02-09 15:09:13 -0700

    Added a clear button

--- app/page.js
+++ app/page.js
@@ -4,7 +4,9 @@ import { mapData } from "./mapData";
 import styles from "./page.module.css";
 import { useState } from "react";
 
-const STARTING_LOCATION = "camp";
+function capitalizeFL(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
 
 export default function Home() {
   const [currentLocation, setCurrentLocation] = useState(); // ["camp", "forest", "lake", "river", "beach", "plains", "desert", "cliffs", "caves"
@@ -21,15 +23,25 @@ export default function Home() {
         {Object.entries(mapData).map(([location]) => (
           <button key={location} onClick={() => {
             handleLocationChange(location);
-          }}>{location}</button>
+          }}>{capitalizeFL(location)}</button>
         ))}
+        <button 
+          onClick={() => {
+            setCurrentLocation(null);
+            setExplorationOptions([]);
+          }}
+          className="clear"
+          style={{ marginLeft: '2rem' }}
+        >
+          Clear
+        </button>
       </div>
 
       <div>
         <h1>Click on your current location to see exploration options here:</h1>
         <h2>{`Current location: ${currentLocation || 'none'}`}</h2>
         {explorationOptions.map((location) => (
-          <h4 key={location} onClick={() => handleLocationChange(location)}>{location}</h4>
+          <h4 key={location} onClick={() => handleLocationChange(location)}>{capitalizeFL(location)}</h4>
         ))}
       </div>
     </main>
